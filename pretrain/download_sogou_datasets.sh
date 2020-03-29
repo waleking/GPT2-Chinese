188912717a4d3c308094bbf0c287d0050b73355e
waleking <huangwaleking@gmail.com>
2020-03-29 16:17:00 -0400

    download SogouCS datasets, and SogouCA datasets

---
+++ pretrain/download_sogou_datasets.sh
@@ -0,0 +1,5 @@
+# To generate the password, visit https://www.sogou.com/labs/resource/cs.php
+wget --user huangwaleking@gmail.com --password YxGSYYVRTLbL64Zz http://www.sogou.com/labs/sogoudownload/SogouCS/SogouCS.tar.gz
+wget --user huangwaleking@gmail.com --password YxGSYYVRTLbL64Zz http://www.sogou.com/labs/sogoudownload/SogouCA/SogouCA.tar.gz
+wget --user huangwaleking@gmail.com --password YxGSYYVRTLbL64Zz http://www.sogou.com/labs/sogoudownload/SogouCS/news_sohusite_xml.full.tar.gz
+wget --user huangwaleking@gmail.com --password YxGSYYVRTLbL64Zz http://www.sogou.com/labs/sogoudownload/SogouCA/news_tensite_xml.full.tar.gz
