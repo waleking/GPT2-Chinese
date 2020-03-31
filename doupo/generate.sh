4d55d252617b2cfb16c7bf8f9c05aad8aeda5f87
waleking <huangwaleking@gmail.com>
2020-03-31 15:06:20 -0400

    add a generate.sh script to generate samples

---
+++ doupo/generate.sh
@@ -0,0 +1,18 @@
+job_dir="doupo"
+
+cd ..
+
+if [ ! -e $job_dir/outputs ]; then
+    mkdir $job_dir/outputs
+fi
+
+python generate.py \
+    --model_path $job_dir/model/final_model/ \
+    --model_config $job_dir/model/final_model/model_config.json \
+    --tokenizer_path $job_dir/vocab.txt \
+    --temperature 0.8 \
+    --length 50 \
+    --topk 50 \
+    --nsamples 50 \
+    --save_samples \
+    --save_samples_path $job_dir/outputs/generated.txt
